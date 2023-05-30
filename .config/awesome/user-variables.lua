e5b151c0ca990c00fdf4c6a3916b02387aba8101
Von Mueller <vonjonmueller@gmail.com>
2023-05-30 03:06:48 -0700

    Add user-variables.lua

---
+++ .config/awesome/user-variables.lua
@@ -0,0 +1,10 @@
+local home = os.getenv('HOME')
+
+local _M = {
+
+    terminal = 'wezterm',
+    modkey = 'Mod4', -- TODO: Double check modkey
+
+    -- user defined wallpaper
+    --wallpaper = home .. "/Pictures/your-wallpaper-here.jpg",
+}
